8409eedfea4d0d7d9c9210f574f06bb1c3063891
neil3d <neil3d@126.com>
2017-12-12 21:21:07 +0800

    init ui states

--- MainForm.cs
+++ MainForm.cs
@@ -12,6 +12,19 @@ namespace excel2json {
     public partial class MainForm : Form {
         public MainForm() {
             InitializeComponent();
+
+            //-- componet init states
+            this.comboBoxType.SelectedIndex = 0;
+            this.comboBoxLowcase.SelectedIndex = 1;
+            this.comboBoxHeader.SelectedIndex = 1;
+
+            this.comboBoxEncoding.Items.Clear();
+            foreach (EncodingInfo ei in Encoding.GetEncodings()) {
+                Encoding e = ei.GetEncoding();
+                this.comboBoxEncoding.Items.Add(e.EncodingName);
+                System.Diagnostics.Debug.Print(e.EncodingName);
+            }
+            this.comboBoxEncoding.SelectedIndex = this.comboBoxEncoding.Items.Count - 1;
         }
 
         private void panelExcelDropBox_DragDrop(object sender, DragEventArgs e) {
