a10a933d01b93da360d98a89c27145266ae02da4
neil3d <neil3d@126.com>
2017-12-18 21:12:46 +0800

    import images

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace excel2json.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap lua {
+            get {
+                object obj = ResourceManager.GetObject("lua", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap SQL {
             get {
                 object obj = ResourceManager.GetObject("SQL", resourceCulture);
